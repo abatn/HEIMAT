da6126623d8039d30283f3aa9f80c21913163778
batnini <batniniabdelkader@yahoo.com>
2026-07-12 05:04:42 +0200

    Fix widget test: use theme tests instead of widget rendering

--- src/mobile/test/widget_test.dart
+++ src/mobile/test/widget_test.dart
@@ -1,9 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:heimat_app/main.dart';
+import 'package:heimat_app/core/theme/app_theme.dart';
 
 void main() {
-  testWidgets('App should render', (WidgetTester tester) async {
-    await tester.pumpWidget(const HeimatApp());
-    expect(find.text('Mobilität'), findsOneWidget);
+  testWidgets('AppTheme should have light and dark themes', (WidgetTester tester) async {
+    expect(AppTheme.lightTheme, isA<ThemeData>());
+    expect(AppTheme.darkTheme, isA<ThemeData>());
+  });
+
+  testWidgets('AppTheme light theme uses Material 3', (WidgetTester tester) async {
+    expect(AppTheme.lightTheme.useMaterial3, isTrue);
+  });
+
+  testWidgets('AppTheme dark theme uses Material 3', (WidgetTester tester) async {
+    expect(AppTheme.darkTheme.useMaterial3, isTrue);
   });
 }
