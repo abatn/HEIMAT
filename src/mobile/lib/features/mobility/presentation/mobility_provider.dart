cd9c57645df641c10f827716b9a5aaf46cc0e93c
abatn <abatn@users.noreply.github.com>
2026-07-13 17:50:14 +0200

    Fix: remove unused flutter_map import

--- src/mobile/lib/features/mobility/presentation/mobility_provider.dart
+++ src/mobile/lib/features/mobility/presentation/mobility_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import '../../../core/config/app_config.dart';
 
